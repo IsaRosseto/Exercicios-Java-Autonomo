66c55b2af5403420584d0472c20307fa3aeb2b59
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-07 19:29:16 -0300

    Update calculoCombustivel.java

--- calculoCombustivel.java
+++ calculoCombustivel.java
@@ -1,5 +1,5 @@
 /*****************************************************************************************************************
-* 5) Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz
+* 	5) Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz
 * 	12 Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto na viagem e a velocidade média.
 * 	Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO * VELOCIDADE.
 * 	Tendo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a
