7ed8277fc46037598b01752bc8af0ed4b758f469
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-08 21:50:45 -0300

    Update Fahrenheit-Celsius.java

--- Fahrenheit-Celsius.java
+++ Fahrenheit-Celsius.java
@@ -1,7 +1,7 @@
 /*****************************************************************************************************************
-* 7) Ler uma temperatura em graus Fahrenheit e apresentá-Ia convertida em graus Celsius. A fórmula de conversão de
-* 		temperatura a ser utilizada é C = (F - 32) * 5 / 9, em que a variável F é a temperatura em graus Fahrenheit e a
-* 		variável C é a temperatura em graus Celsius.
+* 	- Ler uma temperatura em graus Fahrenheit e apresentá-Ia convertida em graus Celsius. A fórmula de conversão de
+* 	temperatura a ser utilizada é C = (F - 32) * 5 / 9, em que a variável F é a temperatura em graus Fahrenheit e a
+* 	variável C é a temperatura em graus Celsius.
 *****************************************************************************************************************/
 
 import java.util.Scanner;
