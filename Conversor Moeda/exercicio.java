64db9edf7e384e7b7fe4bb5313569e560d3a6f8e
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-08 21:52:16 -0300

    Update and rename Conversor$DOLAR$REAL.java to Conversor Moeda/exercicio.java

--- Conversor Moeda/exercicio.java
+++ Conversor Moeda/exercicio.java
@@ -9,7 +9,7 @@
  
  import java.util.Scanner;
 
-public class ConversorDolarReal {
+public class exercicio {
 
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
