7947d36647f9027d3087ad922077f8a3b9af43f5
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-10 09:57:31 -0300

    Update lata de oleo.java

--- lata de oleo.java
+++ lata de oleo.java
@@ -1,7 +1,7 @@
 /*****************************************************************************************************************
-* 8) Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula:
-* 		V = 3.14159 * R * R * A
-* 		Onde as variáveis: V, R e A representam respectivamente o volume, o raio e a altura.
+* 	- Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula:
+* 	V = 3.14159 * R * R * A
+* 	Onde as variáveis: V, R e A representam respectivamente o volume, o raio e a altura.
 *****************************************************************************************************************/
 
 import java.util.Scanner;
