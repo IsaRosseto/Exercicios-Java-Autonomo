e4a3bad3f85effa3a0629256d160f19c5fe64a80
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-12 08:44:03 -0300

    Update Exercicio.java

--- Exercicio.java
+++ Exercicio.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* 11) Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos
+* - Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos
 * valores de modo que a variável A passe a possuir o valor da variável B, e a
 * variável B passe a possuir o valor da variável A. Apresentar os valores
 * trocados.
