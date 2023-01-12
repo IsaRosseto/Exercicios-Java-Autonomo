4884ed9b902bebaeefde73c1cbe18f37b4721371
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-12 08:44:33 -0300

    Update Exercicio.java

--- Exercicio.java
+++ Exercicio.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
-* - Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos
-* valores de modo que a variável A passe a possuir o valor da variável B, e a
-* variável B passe a possuir o valor da variável A. Apresentar os valores
-* trocados.
+*   - Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos
+*   valores de modo que a variável A passe a possuir o valor da variável B, e a
+*   variável B passe a possuir o valor da variável A. Apresentar os valores
+*   trocados.
 *******************************************************************************/
 
 import java.util.Scanner;
