6c573339df8de135b416eb3bc15af75dc3b789b3
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-13 07:58:58 -0300

    Update modulo numero.java

--- modulo numero.java
+++ modulo numero.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
-*   - Escreva um programa que leia um número inteiro e exiba o seu módulo.
-*   O módulo de um número x é:
-*   x se x é maior ou igual a zero
-*   x * (-1) se x é menor que zero
+*      - Escreva um programa que leia um número inteiro e exiba o seu módulo.
+*      O módulo de um número x é:
+*      x se x é maior ou igual a zero
+*      x * (-1) se x é menor que zero
 *******************************************************************************/
 
 import java.util.Scanner;
