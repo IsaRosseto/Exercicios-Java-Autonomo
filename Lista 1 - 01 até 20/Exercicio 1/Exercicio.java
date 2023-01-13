75794ce577f8efc84401dc36cd18ce82266d2c48
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-13 08:02:34 -0300

    Version 1.02.1

--- Lista 1 - 01 até 20/Exercicio 1/Exercicio.java
+++ Lista 1 - 01 até 20/Exercicio 1/Exercicio.java
@@ -1,7 +1,9 @@
 /*******************************************************************************
+
 * 	- Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e 
 * 	dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano
 *	 com 365 dias e mês com 30 dias.
+
 *******************************************************************************/
 
 import java.util.Scanner;
