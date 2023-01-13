9eec6f95f668fa96c26b1166c0c212d4be451a96
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-01-13 08:00:37 -0300

    Version 1.02.0

--- Modulo de numero/Exercicio.java
+++ Modulo de numero/Exercicio.java
@@ -1,8 +1,10 @@
 /*******************************************************************************
+
 *      - Escreva um programa que leia um número inteiro e exiba o seu módulo.
 *      O módulo de um número x é:
 *      x se x é maior ou igual a zero
 *      x * (-1) se x é menor que zero
+
 *******************************************************************************/
 
 import java.util.Scanner;
