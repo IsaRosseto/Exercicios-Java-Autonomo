d2d387921d102cf171d0b8cb869a710a6ecd0481
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-02-01 09:21:32 -0300

    Update and rename Lista 1 - 01 até 20/09/Exercicio.java to Lista 1 - 01 até 20/09Exercicio.java

--- Lista 1 - 01 até 20/09Exercicio.java
+++ Lista 1 - 01 até 20/09Exercicio.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* 16) Escreva um programa que leia dois números e exiba mensagem informando o
+* - Escreva um programa que leia dois números e exiba mensagem informando o
 * valor do maior número e o valor do menor número. Se os dois números forem
 * iguais, o programa deve exibir mensagem informando este fato.
 *******************************************************************************/
